5a8ca8caafc52ae91a648b4e92652078f7320ed1
Joyce Lin <joycelin79@gmail.com>
2019-11-11 11:48:22 -0800

    update spectral yaml

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,7 @@ set -e
 # # [GOOD PRACTICE] run your tests against a local server
 # # requires your Postman collection and environment files to be located within the project directory
 # # formatted like: newman run <name-of-collection.json> -e <name-of-environment.json>
-# newman run petstore_postman_collection.json
+# newman run Cosmos.postman_collection.json -e cosmos.postman_environment.json
 
 # # [BETTER PRACTICE] run your tests using the Postman API (https://docs.api.getpostman.com/) to retrieve the latest versions of your collection and environment
 # # requires collection UID, environment UID, and your Postman API key
@@ -22,7 +22,7 @@ newman run https://api.getpostman.com/collections/${POSTMAN_COLLECTION_UID}?apik
 # # [GOOD PRACTICE] lint a local specification file
 # # requires your API specification file to be located within the project directory
 # # formatted like: spectral lint <name-of-specification.yaml>
-# spectral lint petstore.yaml
+# spectral lint cosmos.yaml
 
 # # [BETTER PRACTICE] lint the file using the Postman API (https://docs.api.getpostman.com/) to retrieve the latest version of your specification file
 # # requires your API schema path and your Postman API key
